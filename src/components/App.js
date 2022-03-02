95850bcff42cfee369d7da25925fb2a3568d85fd
Vivian Gomez <viviangomez0@gmail.com>
2022-03-01 21:32:53 -0500

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+// import header 
+import Header from "./Header";
+import Filter from "./Filter"
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [items, setItems] = useState(itemData); 
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+      <Header onDarkModeClick={handleDarkModeClick} mode={isDarkMode}/>
       <ShoppingList items={itemData} />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+function Header ({onDarkModeClick,mode}){
+
+return(
+    <header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {mode ? "Dark" : "Light"} Mode
+        </button>
+    </header>
+
+)
+
+
+}
+
+
+export default Header; 
